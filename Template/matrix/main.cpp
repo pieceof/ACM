cd8bff5e34f05553a87920b236e6445b1fc933a7
bitwater <bitwater1997@gmail.com>
2017-08-19 19:38:33 +0800

    修改矩阵快速幂模板

--- Template/matrix/main.cpp
+++ Template/matrix/main.cpp
@@ -1,32 +1,38 @@
-#include <bits/stdc++.h>
-#define CLEAR(a) memset((a),0,sizeof(a))
-#define FLAG(a) memset((a) , -1 , sizeof(a))
-#define  varName(x) #x
-#define  deBug(exp) cout<< " == > " << #exp<<" : "<< (exp) << endl
-#define Case printf("Case %d: ",kase++)
-#define Ans(x) printf("%d\n",(x))
-#define readfile(path) freopen( (path) , "r", stdin )
-#define writefile(path) freopen( (path) , "w", stdout )
-using namespace std;
-
-typedef long long llt;
-const int MOD = 1000000007;
-const int Cube_SIZE = 2;///¾ØÕó´óÐ¡
+#include"../../headers.h"
+const int MOD = 10000007;
+const int Cube_SIZE = 15;   //矩阵大小
 struct Cube{
+    int spec;
     llt mat[Cube_SIZE][Cube_SIZE];
 };
 //单位矩阵
-Cube const _UnitCube = {
-    1, 0,
-    0, 1,
+Cube const _UnitCube = {15,
+{
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+}
 };
 ///矩阵乘法
-Cube _Multiply(Cube A,Cube B,llt mod){
+Cube _Multiply(Cube const&A,Cube const&B,llt mod){
     Cube _tmpCube;
-    for (int i = 0;i < Cube_SIZE;++i)
-    for (int j = 0;j < Cube_SIZE;++j){
+    _tmpCube.spec = A.spec;
+    for (int i = 0;i < A.spec;++i)
+    for (int j = 0;j < A.spec;++j){
         _tmpCube.mat[i][j] = 0;
-        for (int k = 0;k < Cube_SIZE;++k){
+        for (int k = 0;k < A.spec;++k){
             _tmpCube.mat[i][j] += A.mat[i][k]*B.mat[k][j];
             _tmpCube.mat[i][j] %= mod;
         }
@@ -34,9 +40,10 @@ Cube _Multiply(Cube A,Cube B,llt mod){
     return _tmpCube;
 }
 ///矩阵快速幂
-Cube power_Cube(Cube A,llt n,llt mod) 
+Cube power_Cube(Cube &A,llt n,llt mod)
 {
     Cube _tmpCube = _UnitCube;
+    _tmpCube.spec = A.spec;
     while(n){
         if(n & 1)
             _tmpCube = _Multiply(_tmpCube, A ,mod);
@@ -46,29 +53,36 @@ Cube power_Cube(Cube A,llt n,llt mod)
     return _tmpCube;
 }
 llt n,m;
+int nub[15] = {23};
 int main(){
     Cube arr;
-    int t;scanf("%d",&t);
-    while ( t-- ){
-        scanf("%lld%lld",&n,&m);
-        llt ans = 0;
-        arr.mat[0][0] = 0;arr.mat[0][1] = 1;
-        arr.mat[1][0] = 2;arr.mat[1][1] = 1;
-        arr = power_Cube(arr,n,MOD);
-        if( n % 2 == 0 ){
-            arr.mat[0][0] -= 1;
-            arr.mat[1][1] -= 1;
-        }else {
-            arr.mat[0][0] -= -1;
-            arr.mat[0][1] -= 1;
-            arr.mat[1][0] -= 2;
-            arr.mat[1][1] -= 0;
+//    int t;scanf("%d",&t);
+    while ( scanf("%I64d%I64d",&n,&m) != EOF ){
+        for (int i = 1 ;i <= n;++i )
+            scanf("%d",nub+i);
+        nub[0] = 23;
+        nub[++n] = 3; n++;
+        for (int i = 0;i < n-1;++i ){
+            arr.mat[i][0] = 10;
+            for (int j = 1;j < n-1;++j ){
+                if ( j > i ) arr.mat[i][j] = 0;
+                else arr.mat[i][j] = 1;
+            }
+            arr.mat[i][n-1] = 1;
         }
-        arr = power_Cube(arr,m-1,MOD);
-        ans += (arr.mat[0][0] + arr.mat[0][1] )%MOD;
-        ans %= MOD;
+        arr.spec = n;
+        for (int i = 0;i < n;++i )
+            arr.mat[n-1][i] = 0;
+        arr.mat[n-1][n-1] = 1;
+//        ArrayDisp( (llt*)arr.mat,n,n,15);
+        arr = power_Cube(arr,m,MOD);
+//        ArrayDisp( (llt*)arr.mat,n,n,15);
+        llt ans = 0;
+        for (int i = 0;i < n;++i  ){
+            ans += (nub[i]*arr.mat[n-2][i])%MOD;
+            ans %= MOD;
+        }
         printf("%lld\n",ans);
-//        cout <<ans<<endl;
     }
     return 0;
 }
