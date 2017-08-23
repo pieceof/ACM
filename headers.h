cfb69bb9f3d72ff486f8ea037043f3a0a99f181e
bitwater <bitwater1997@gmail.com>
2017-08-23 09:42:49 +0800

    修改头文件

--- headers.h
+++ headers.h
@@ -1,31 +1,30 @@
 #include <cstdio>
-#include <set>
-#include <algorithm>
-#include <cstring>
-#include <cmath>
 #include <iostream>
-#include <climits>
-#include <assert.h>
-#include <string>
-#include <map>
-#include <vector>
 #include <iomanip>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
 #include <set>
 #include <queue>
 #define CLEAR(a) memset((a),0,sizeof(a))
-#define FLAG(a) memset((a) , -1 , sizeof(a))
-#define  varName(x) #x
-#define  deBug(exp) cout<< " == > " << #exp<<" : "<< (exp) << endl
-#define  deBugPair(exp1,exp2) cout<< " == > " << "(" << #exp1<<" : "<< (exp1) << "," << #exp2 << " : " << (exp2) << ")" << endl
-#define Case printf("Case %d:\n",kase++)
+#define FLAG(a) memset((a),-1,sizeof(a))
+#define varName(x) #x
+#define deBug(exp) cout<< " == > " << #exp<<" : "<< (exp) << endl
+#define deBugPair(exp1,exp2) cout<< " == > " << "(" << #exp1<<" : "<< (exp1) << "," << #exp2 << " : " << (exp2) << ")" << endl
+#define Case   printf("Case %d:\n",kase++)
 #define Ans(x) printf("%d\n",(x))
-#define readfile(path) freopen( (path) , "r", stdin )
+#define readfile(path)  freopen( (path) , "r", stdin )
 #define writefile(path) freopen( (path) , "w", stdout )
 using namespace std;
 ///-------------------------   math ------------------------///
 typedef long long llt;
-const double eps = 1e-9;
-const double PI = acos(-1);
+double const PI = acos(-1.0);
 inline double ln(double const&x ){ return log(x)/log(exp(1));}
 inline double Vfrustum( double const&r1,double const&r2,double const&h ){
     return PI * h * ( r1*r1 + r2*r2 + r1*r2 )/3;
@@ -41,7 +40,6 @@ void ArrayCin( T *a ,int n ,int pos = 0){
 
 template <typename T>
 void ArrayDisp(T *a,int n ){
-//    std::cout << varName(a) << "  : ";
     cout << "===> : ";
     for (int i = 0;i < n;++i )cout << std::right << setw(2)<<a[i]<<" ";cout << endl;
 }
@@ -49,7 +47,7 @@ template <typename T>
 void ArrayDisp(T *a,int n,int m ,int twoLen){
     for (int i = 0;i < n;++i ){
         for (int j = 0;j < m;++j ){
-            cout << std::right << setw(2)<< *(a +i*twoLen +j) << " ";
+            cout << std::right << setw(2)<< *( a + i*twoLen + j ) << " ";
         }cout << endl;
     }cout << endl;
 }
@@ -61,9 +59,8 @@ namespace fastIo{
         char c;
         int sgn;
         if (c = getchar(), c == EOF) return 0; //EOF
-        while (c != '-' && (c < '0' || c > '9') ) {
+        while (c != '-' && (c < '0' || c > '9') )
             if((c = getchar()) == EOF) return 0;
-        }
         sgn = (c == '-') ? -1 : 1;
         ret = (c == '-') ? 0 : (c - '0');
         while (c = getchar(), c >= '0' && c <= '9') ret = ret * 10 + (c - '0');
