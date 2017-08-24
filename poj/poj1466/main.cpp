c93d53e1fe5d6c3ae069de6709e78acf68dd4cc6
bitwater <bitwater1997@gmail.com>
2017-08-24 21:34:28 +0800

    增加poj1466 Girls and Boys [二分图 最大独立集 ]

---
+++ poj/poj1466/main.cpp
@@ -0,0 +1,83 @@
+#include "../../headers.h"
+
+
+typedef int weight_t;
+/** \二分图最大匹配（匈牙利算法的DFS实现）
+ * 临界表 无向边(ans/2)
+ * CALL:ret=hungary();输出最大匹配数
+ * 时间复杂度:O(VE);
+ *********
+ */
+const int SIZE_V = 2000+500;
+const int SIZE_E = 30000+500;
+struct edge_t{
+    int node;
+    int next;
+}Edge[SIZE_E];
+int Ecnt ; // = 0
+int Ver[SIZE_V];
+void mkEdge( int a,int b ){
+    Edge[Ecnt].node = b;
+    Edge[Ecnt].next = Ver[a];
+    Ver[a] = Ecnt++;
+
+    Edge[Ecnt].node = a;
+    Edge[Ecnt].next = Ver[b];
+    Ver[b] = Ecnt++;
+}
+void init_Graph(){
+    Ecnt = 1;
+    CLEAR(Ver);
+}
+int Link[SIZE_V]; //对于右边集合的 i, link[i]即对应左边集合的 node
+bool vis[SIZE_V];
+/**< 对于左边的集合的node节点 求增广路径 */
+bool DFS( int u){
+    /**< 对右边集合的每个结点遍历一次 */
+    for (int next = Ver[u];next;next = Edge[next].next ){
+        int v = Edge[next].node;
+        /**< 如果已经找过 */
+        if ( vis[v] )continue; vis[v] = true;
+        /**< 如果这个点上未盖点 或者该点还能找到增广路径 */
+        if ( Link[v] == -1 || DFS( Link[v] ) ){
+            Link[v] = u;
+            return true;
+        }
+    }
+    return false;
+}
+
+int hungary(int n ){
+    int ret = 0;
+    memset(Link , -1 ,sizeof(Link));
+    /**< 对左边集合的每个点求增广路径 */
+    for (int u = 1;u <= n;++u){
+        memset(vis,0,sizeof(vis));
+        if (DFS(u)) ret++;
+    }
+    return ret;
+}
+
+
+int n,k;
+char ch[10];
+int main(){
+//    readfile("in.txt");
+    while ( scanf("%d",&n) != EOF ){
+        int x,y;
+        init_Graph();
+        for (int i = 0;i < n;++i ){
+            scanf("%s",ch);scanf(" (%d) ",&k);
+//            k = ch[1]-'0';
+            while ( k-- ){
+                scanf("%d",&x);
+                mkEdge(i+1,x+1);
+            }
+        }
+        int ans = hungary(n)/2;
+//        deBug(ans);
+//        deBug( n-ans );
+        Ans(n-ans);
+    }
+    return 0;
+}
